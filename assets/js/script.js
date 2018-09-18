2d1d60d996eb866a2e23943a7ed539c6292fbf3b
sarahtencher <sarahtencher@gmail.com>
2018-09-17 21:52:43 -0700

    styles for detail pages, ruined home list-items

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-links = document.querySelectorAll(".ga-link");
-links.forEach(function (element) {
-  element.addEventListener("click", function() {
-    var name = element.getAttribute("ga-name");
-    ga('send','event','Outbound Link','click', name);
-  })
-});
